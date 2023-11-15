314cd50082e81625298af60e27a5be4696436e47
yaroslavzefi <yaroslavzef@gmail.com>
2023-11-15 07:37:49 +0300

    routes fix

--- src/shared/config/RouteConfing/RouteConfing.tsx
+++ src/shared/config/RouteConfing/RouteConfing.tsx
@@ -1,22 +1,21 @@
-import {RouteProps} from "react-router";
+import {RouteProps} from "react-router-dom";
 import {Main} from "pages/Main";
 import {About} from "pages/About";
 
 export enum AppRoutes {
-   MAIN = 'main',
+    MAIN = 'main',
     ABOUT = 'about',
 }
 
+
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: 'about,',
+    [AppRoutes.ABOUT]: 'about',
 }
 
-
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-
     [AppRoutes.MAIN]: {
-       path: RoutePath.main,
+        path: RoutePath.main,
         element: <Main />
     },
     [AppRoutes.ABOUT]: {
