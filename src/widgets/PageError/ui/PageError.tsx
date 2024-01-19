45647ee51b17fa3d50067e7714647be7eef4178b
yaroslavzefi <yaroslavzef@gmail.com>
2024-01-19 09:57:03 +0300

    page-error refactor

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,31 +1,26 @@
 import { classNames } from 'helpers/classNames/classNames'
-import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Button } from 'shared/ui/Button/Button'
 
 import styles from './PageError.module.scss'
 
-interface PageErrorProps {
-  className?: string
+export const PageError = () => {
+    const { t } = useTranslation()
 
-}
-export const PageError = ({ className }: PageErrorProps) => {
-  const { t } = useTranslation()
+    const reloadPageHandler = () => {
+        location.reload()
+    }
 
-  const reloadPageHandler = () => {
-    location.reload()
-  }
-
-  return (
-      <div className={classNames(styles.PageError, {}, [])}>
-          <h1>
-              {t('Произошла непредвиденная ошибка!')}
-          </h1>
-          <img
-              src="https://static9.tgcnt.ru/posts/_0/29/29464aa629986201f22bed32f420f80e.jpg"
-              alt="error-img"
-          />
-          <Button onClick={reloadPageHandler}>{t('Перезагрузить страницу')}</Button>
-      </div>
-  )
+    return (
+        <div className={classNames(styles.PageError, {}, [])}>
+            <h1>
+                {t('Произошла непредвиденная ошибка!')}
+            </h1>
+            <img
+                src="https://static9.tgcnt.ru/posts/_0/29/29464aa629986201f22bed32f420f80e.jpg"
+                alt="error-img"
+            />
+            <Button onClick={reloadPageHandler}>{t('Перезагрузить страницу')}</Button>
+        </div>
+    )
 }
