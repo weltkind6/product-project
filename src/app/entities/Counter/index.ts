a9aec55fe1fd344004877eb98738cb57c5750782
yaroslavzefi <yaroslavzef@gmail.com>
2024-02-06 09:18:12 +0300

    remove: unused imports

--- src/app/entities/Counter/index.ts
+++ src/app/entities/Counter/index.ts
@@ -1,5 +1,3 @@
 import {Counter} from "./ui/Counter";
-import {counterReducers} from "./model/slice/CounterSlice";
-import type {CounterSchema} from "./model/types/CounterSchema";
 
 export {Counter};
